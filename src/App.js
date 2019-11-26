a38cf71dc0308fb83d2977babf3ea6813725f880
mateusisrael <mateuss.i.fernandes@gmail.com>
2019-11-25 22:02:42 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Lampada from './components/Lampada';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Lampada/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
